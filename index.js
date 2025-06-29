99f667db8e1dc8e135cd265a9104f7786343cd25
Vivian Gichure <viviangichure@gmail.com>
2025-06-29 12:44:32 +0300

    completed

---
+++ index.js
@@ -0,0 +1,19 @@
+// Constructor for Scooter
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+// Constructor for Driver
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+// Constructor for PickupLocation
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
